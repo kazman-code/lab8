67c913652ade346395b77699d9e9a63a4955390e
Mick_code <mickdonnellan2@gmail.com>
2026-04-19 23:47:00 +0100

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,12 +10,14 @@ import { Storage } from '@ionic/storage-angular';
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonLabel, RouterLink],
 })
 export class HomePage {
+  //holds the stored value gotten from ionic storage
   myStatus:string ="";
+  //injects ionic storage service into the component
   constructor(private storage:Storage) {}
-
+//async method that runs when page is active
   async ionViewWillEnter(){
-    await this.storage.create();
-    this.myStatus = await this.storage.get('myStatus')
+    await this.storage.create();//makes sure storage is initialised
+    this.myStatus = await this.storage.get('myStatus')//gets saved values
 
   }
 }
